661380e2e8b0429fa505ecfad91fe7e2a4c827eb
wlz <lingze.wl@gmail.com>
2023-03-13 21:50:10 +0800

    feat(use service)
    * use services save and get config in web server setup

--- src/main.rs
+++ src/main.rs
@@ -7,22 +7,30 @@ use std::{io, net::SocketAddr};
 
 use axum::{routing::get, Router};
 use axum_server::{bind, Handle};
-use config::Config;
+use service::{init_service, services};
 
 #[tokio::main]
 async fn main() {
     let config = config::parse();
 
     // set config to service
+    if let Err(e) = init_service(config) {
+        eprintln!("It looks like your config is invalid: {}", e);
+        std::process::exit(-1);
+    };
 
     // start web server
     let start = async {
-        run_server(config).await.unwrap();
+        run_server().await.unwrap();
     };
     start.await;
 }
 
-async fn run_server(config: Config) -> io::Result<()> {
+async fn run_server() -> io::Result<()> {
+    // get config from service
+    let config = &services().config;
+
+    // start web server
     let addr = SocketAddr::from((config.address, config.port));
     let hander = Handle::new();
     let app = routes().into_make_service();
