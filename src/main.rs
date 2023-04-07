25dfc91956843d6f8653abf7893fa342dd586de7
wlz <lingze.wl@gmail.com>
2023-04-07 14:43:37 +0800

    feat(tracing implement loging)

--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,27 @@ mod utility;
 
 use std::{io, net::SocketAddr};
 
-use axum::{routing::get, Router};
+use axum::{
+    extract::MatchedPath,
+    http,
+    routing::{get, post},
+    Router,
+};
 use axum_server::{bind, Handle};
 use service::{init_service, services};
+use tower::ServiceBuilder;
+use tower_http::trace::TraceLayer;
 
 #[tokio::main]
 async fn main() {
+    // set tracing log
+    tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::INFO)
+        .init();
+
     let config = config::parse();
+
+    tracing::info!("config: {:?}", config);
 
     // set config to service
     if let Err(e) = init_service(config) {
@@ -34,7 +48,19 @@ async fn run_server() -> io::Result<()> {
     // start web server
     let addr = SocketAddr::from((config.address, config.port));
     let hander = Handle::new();
-    let app = routes().into_make_service();
+
+    let middleware = ServiceBuilder::new().layer(TraceLayer::new_for_http().make_span_with(
+        |request: &http::Request<_>| {
+            let path = if let Some(path) = request.extensions().get::<MatchedPath>() {
+                path.as_str()
+            } else {
+                request.uri().path()
+            };
+            tracing::info_span!("http_request", %path)
+        },
+    ));
+
+    let app = routes().layer(middleware).into_make_service();
     bind(addr).handle(hander).serve(app).await?;
 
     Ok(())
