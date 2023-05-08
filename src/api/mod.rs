c1efbd30a834483de16c667f46e9cd0214c1350c
wlz <lingze.wl@gmail.com>
2023-05-09 00:08:42 +0800

    fixup(We need to modify the == to != in this judge) This change will reverse the logic.

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -16,7 +16,7 @@ pub async fn ping() -> &'static str {
 }
 
 fn token_check(token: String, user: String) -> Result<()> {
-    (get_user_from_token(token)? == user)
+    (get_user_from_token(token)? != user)
         .then_some(())
         .ok_or(Error::BadRequest("Wrong token."))
 }
